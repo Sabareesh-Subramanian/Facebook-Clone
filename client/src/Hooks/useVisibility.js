05a4632acabd42274c2a67d7e1d1ee54c4fb000f
imankush7 <ankushkrsingh96@gmail.com>
2021-11-12 19:51:21 +0530

    Login signup

---
+++ client/src/Hooks/useVisibility.js
@@ -0,0 +1,28 @@
+import {useState} from "react"
+
+function useVisibility() {
+
+    const [state,setState] = useState(false)
+    const handleToggle = (e)=>{
+        e &&  e.stopPropagation();
+        setState(state=>!state);
+    }
+
+    const handleOpen = (e)=>{
+        e &&  e.stopPropagation();  
+        setState(true);
+    }
+
+    const handleClose = (e)=>{
+        e &&  e.stopPropagation();
+        setState(false);
+    }
+
+    window.addEventListener("click",()=>{
+        setState(false)
+    })
+    
+    return [state,handleToggle,handleClose,handleOpen]
+}
+
+export default useVisibility
