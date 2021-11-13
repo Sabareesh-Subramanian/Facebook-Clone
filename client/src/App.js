2ef97749ccd2b0cec81581767840b5d0f0e8bae7
Sabareesh Subramanian <sabareesh16797@gmail.com>
2021-11-13 10:17:49 +0530

    Resolving a part of the conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,11 @@
-
-import './App.css';
-
-import LoginPage from './Components/Login/LoginPage';
-import { AllRoutes } from './Routes/Routes';
+import "./App.css";
+import { AllRoutes } from "./Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes/>
-
-import { Route, Switch } from "react-router-dom";
-import { UserProfile } from "./Components/UserProfile/UserProfile";
-
-function App() {
-  return (
-    <div>
-      <Switch>
-    <Route path="/" exact>
-    <LoginPage/>
-    </Route>
-        <Route path="/profile">
-          <UserProfile />
-        </Route>
-      </Switch>
-
+      <AllRoutes />
     </div>
-
   );
 }
 
